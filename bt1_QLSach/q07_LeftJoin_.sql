c5ffbeeb151f875e55316402e0645e11047563a0
Buituananh3101 <31012006github@gmail.com>
2026-02-07 13:47:24 +0700

    Xong BT QLBanSach Lan 1

--- bt1_QLSach/q07_LeftJoin_.sql
+++ bt1_QLSach/q07_LeftJoin_.sql
@@ -20,13 +20,13 @@ group by
     n.TenNXB
 
 --c2
-SELECT 
+select 
     n.TenNXB,
-    COUNT(s.MaSach) as TongSoDauSach -- COUNT bỏ qua NULL nên sẽ trả về 0, rất chuẩn
-FROM 
+    count(s.MaSach) as TongSoDauSach -- COUNT bỏ qua NULL nên sẽ trả về 0, rất chuẩn
+from 
     tNhaXuatBan n  -- Lấy bảng NXB làm gốc (bên trái)
-    LEFT JOIN tSach s ON n.MaNXB = s.MaNXB -- Để không bị mất NXB chưa có sách
-GROUP BY 
+    left join tSach s on n.MaNXB = s.MaNXB -- Để không bị mất NXB chưa có sách
+group by 
     n.TenNXB;
 
 --28. Với từng NXB, tìm giá bán cao nhất, thấp nhất, trung bình của các sản phẩm.
@@ -40,3 +40,49 @@ from
     LEFT JOIN tSach s ON n.MaNXB = s.MaNXB -- Để không bị mất NXB chưa có sách
 group by 
     n.TenNXB
+
+--36. Tìm sách có tổng số lượng bán ra thấp nhất trong năm 2014.
+--> Lấy bảng(mã sách, số lượng được bán) --> Lấy top 1
+
+select top 1 with ties
+    s.MaSach,
+    s.TenSach,
+    isnull(sum(ban.SLBan), 0) as TongSL
+from 
+    tSach s
+    left join (
+        -- Lọc riêng dữ liệu bán trong năm 2014 trước
+        select 
+            c.MaSach, 
+            c.SLBan
+        from 
+            tChiTietHDB c 
+            join tHoaDonBan h on c.SoHDB = h.SoHDB
+        where 
+            year(h.NgayBan) = 2014
+    ) as ban on s.MaSach = ban.MaSach
+group by 
+    s.MaSach, 
+    s.TenSach
+order by 
+    TongSL asc;
+
+--46. Đưa ra thông tin toàn bộ sách, nếu sách được bán trong năm 2014 thì đưa ra SL bán
+--> tạo cái bảng (mã sách, số lượng bán 2014) --> left join để lấy thông tin sách
+select 
+    s.*, 
+    ban.TongSLBan
+from 
+    tSach s
+    left join (
+        select 
+            c.MaSach, 
+            sum(c.SLBan) as TongSLBan
+        from 
+            tChiTietHDB c
+            join tHoaDonBan h on c.SoHDB = h.SoHDB
+        where 
+            year(h.NgayBan) = 2014
+        group by 
+            c.MaSach
+    ) as ban on s.MaSach = ban.MaSach;
